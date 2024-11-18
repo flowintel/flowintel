7680d2db4ae02f43c14fff250ee978c17d08f22c
David Cruciani <david.cruciani@circl.lu>
2024-11-18 16:02:39 +0100

    chg: [tools] stats page, like old home page

--- app/tools/tools.py
+++ app/tools/tools.py
@@ -41,7 +41,7 @@ def importer():
 
 @tools_blueprint.route("/module")
 @login_required
-def home():
+def module():
     return render_template("tools/module_index.html")
 
 
@@ -55,3 +55,36 @@ def get_modules():
 def reload():
     get_modules_list()
     return {"message": "Modules reloaded", "toast_class": "success-subtle"}, 200
+
+
+#########
+# Stats #
+#########
+from ..db_class.db import Case, Case_Org, Task, Task_User
+
+@tools_blueprint.route("/stats")
+@login_required
+def stats():
+    cases_open = Case.query.join(Case_Org, Case_Org.case_id==Case.id).where(Case_Org.org_id==current_user.org_id, Case.completed==False).all()
+
+    cases_closed = Case.query.join(Case_Org, Case_Org.case_id==Case.id).where(Case_Org.org_id==current_user.org_id, Case.completed==True).all()
+
+    tasks_open = Task.query.join(Task_User, Task_User.task_id==Task.id).where(Task_User.user_id==current_user.id, Task.completed==False).all()
+    tasks_closed = Task.query.join(Task_User, Task_User.task_id==Task.id).where(Task_User.user_id==current_user.id, Task.completed ==True).all()
+
+    all_cases_open = Case.query.filter_by(completed=False).all()
+    all_cases_closed = Case.query.filter_by(completed=True).all()
+
+    all_tasks_open = Task.query.filter_by(completed=False).all()
+    all_tasks_closed = Task.query.filter_by(completed=True).all()
+    
+    return render_template("tools/stats.html", 
+                           cases_open_len=len(cases_open), 
+                           cases_closed_len=len(cases_closed), 
+                           tasks_open_len=len(tasks_open), 
+                           tasks_closed_len=len(tasks_closed),
+                           all_cases_open=len(all_cases_open),
+                           all_cases_closed=len(all_cases_closed),
+                           all_tasks_open=len(all_tasks_open),
+                           all_tasks_closed=len(all_tasks_closed))
+
