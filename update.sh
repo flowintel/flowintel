9d799f86748ad7bc3c7fdf58f4adaf561716b3b8
David Cruciani <david.cruciani@circl.lu>
2024-11-25 11:14:57 +0100

    chg: [update] set error + echo some text

--- update.sh
+++ update.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+set -e
 source env/bin/activate
 
 ## Get update from git
+echo "##########"
+echo "Git pull"
+echo "##########"
 git pull
 
 ## Copy db for migration
+echo ""
+echo "##########"
+echo "Migration"
+echo "##########"
 if [ ! -d "instance/backup" ]; then
   mkdir instance/backup
 fi
@@ -13,6 +21,11 @@ cp instance/flowintel.sqlite instance/backup/$(date +"%Y_%m_%d").sqlite
 ## Run migration
 ./migrate.sh -u
 
+echo ""
+echo "##########"
+echo "Submodule"
+echo "Update"
+echo "##########"
 ## Update submodules
 git submodule update
 python3 app.py -tg
