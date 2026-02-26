d215d8d8437e022c98127b09bb9dd3396fd20fe5
Koen Van Impe <koen.vanimpe@cudeso.be>
2026-02-26 21:53:06 +0100

    - Avoid double galaxies in case view - Change style of History and Files Tab - Title of modal form

--- app/static/js/case/tags_edition.js
+++ app/static/js/case/tags_edition.js
@@ -119,7 +119,7 @@ export default {
         <div class="modal-dialog modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Tags</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit tags, taxonomies and galaxies</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
