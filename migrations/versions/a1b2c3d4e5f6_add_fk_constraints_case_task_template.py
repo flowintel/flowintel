e28da269685de232bcab1e86899944fadb41b7d8
David Cruciani <david.cruciani@circl.lu>
2026-04-07 13:54:53 +0200

    chg: [migration] head

--- migrations/versions/a1b2c3d4e5f6_add_fk_constraints_case_task_template.py
+++ migrations/versions/a1b2c3d4e5f6_add_fk_constraints_case_task_template.py
@@ -1,7 +1,7 @@
 """Add FK constraints to Case_Task_Template
 
 Revision ID: a1b2c3d4e5f6
-Revises: 3567475b1063
+Revises: f8c4a065648d
 Create Date: 2026-04-04 10:00:00.000000
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'a1b2c3d4e5f6'
-down_revision = '3567475b1063'
+down_revision = 'f8c4a065648d'
 branch_labels = None
 depends_on = None
 
