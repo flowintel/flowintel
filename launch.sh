fb5c9bf16e1fc52e91d22cbe994f119a1eb42d91
Koen Van Impe <koen.vanimpe@cudeso.be>
2026-04-15 21:47:16 +0200

    - Avoid fixed development env variable

--- launch.sh
+++ launch.sh
@@ -72,13 +72,13 @@ function killscript {
 
 function taxo_galaxy_update {
     prepare_app_run
-    export FLASKENV="development"
+    export FLASKENV="${FLASKENV:-development}"
     python3 app.py -utg
 }
 
 function misp_module_update {
     prepare_app_run
-    export FLASKENV="development"
+    export FLASKENV="${FLASKENV:-development}"
     screen -L -Logfile logs/misp.log -dmS "misp_mod_flowintel" bash -c "misp-modules -l 127.0.0.1"
     sleep 3
     python3 app.py -mm
