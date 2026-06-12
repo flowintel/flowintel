5ae340cc05a42f9b37be6378bed9de89009afe9e
Koen Van Impe <koen.vanimpe@cudeso.be>
2026-06-12 10:24:15 +0200

    Creating a custom tag with a name that already existed triggered a UNIQUE constraint violation.
    Add a `validate_name` method on AddCustomTagForm that looks the name
    up in the database before the row is inserted.

--- app/custom_tags/form.py
+++ app/custom_tags/form.py
@@ -4,7 +4,9 @@ from wtforms.fields import (
     SubmitField,
     ColorField
 )
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, Length, ValidationError
+
+from ..db_class.db import Custom_Tags
 
 
 class AddCustomTagForm(FlaskForm):
@@ -12,3 +14,7 @@ class AddCustomTagForm(FlaskForm):
     color = ColorField('Color', validators=[InputRequired()])
     icon = StringField('Icon')
     submit = SubmitField('Create')
+
+    def validate_name(self, field):
+        if field.data and Custom_Tags.query.filter_by(name=field.data.strip()).first():
+            raise ValidationError("A custom tag with this name already exists")
